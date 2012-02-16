3632e644a31c04e67532926dc976961e81afedc1
Noah Hoffman <noah.hoffman@gmail.com>
2012-02-15 20:26:35 -0800

    basic framework in place

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# usage: ./install_all.sh [pip args]
+
+# Install packages specified in ./pypackages
+pip install $* --requirement=packages
+
+# Install each submodule to the current environment.
+git submodule init
+git submodule update --rebase
+git submodule foreach "pip install $* ."
+
+echo "installed packages:"
+lssitepackages
+
+
